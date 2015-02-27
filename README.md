438baa51079c9adf906c2d811eb29b2cb0fb4e4a
thomasgalley <thomas.galley.14@ucl.ac.uk>
2015-02-27 17:50:30 +0000

    Created array of of intial positions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# numpy_boid
+A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
+for use as an exercise on refactoring
---
+++ np_boids.py
@@ -0,0 +1,6 @@
+import numpy as np
+boid_positions=np.random.rand(2,50)
+boid_positions[0,:] *= 500 
+boid_positions[0,:] -= 50 
+boid_positions[1,:] *= 300
+boid_positions[1,:] -= 300 
